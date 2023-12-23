df191eeb6323ebae265bd2a4b792191b4705f60f
Juan Herrera Vázquez <jherrerav1901@alumno.ipn.mx>
2023-12-23 08:23:37 -0600

    Clase 15 terminada

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+
+export const metadata = {
+  title: 'ContactPage',
+  description: 'ContactPage',
+};
+export default function ContactPage()
+{
+  return (
+    <>
+      <span className="text-7xl">Contact Page</span>
+    </>
+  )
+}
---
+++ app/pricing/page.tsx
@@ -0,0 +1,14 @@
+
+export const metadata = {
+  title: 'Pricing Page',
+  description: 'Pricing',
+};
+
+export default function ContactPage()
+{
+  return (
+    <>
+      <span className="text-7xl">Contact Page</span>
+    </>
+  )
+}
